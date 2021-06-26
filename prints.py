86a8d21108459367eb7c111e76af906a8ca6c491
vicious-lu <luis.awsm@gmail.com>
2021-06-25 21:00:54 -0600

    change global configurations

---
+++ prints.py
@@ -0,0 +1,10 @@
+#As simple as that:
+print("Hello World")
+
+# With variables
+word = "Hello World"
+print(word)
+word = 2      #The value can be changed during execution
+print(word)
+# We dont need to specify variable types
+
--- printsAndVariables.py
+++ printsAndVariables.py
@@ -4,5 +4,13 @@ print("Hola mundo")
 #With variables
 word = "Hola mundo"
 print(word)
-word = 2      #The value can be changed during execution
+word = 2      #The value can be changed during execution, and dont need to add a typo
 print(word)
+
+#Simple variable use
+x = 10
+y = 5
+z = x + y
+print(z)
+print(x)
+print(y)
