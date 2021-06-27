e91de61c922ac175d87923efa7e3bd6d8f0bdf04
vicious-lu <luis.awsm@gmail.com>
2021-06-26 19:19:17 -0600

    add a code to check the age of a person to see if is an adult

---
+++ are18.py
@@ -0,0 +1,7 @@
+#code to check if someone is an adult by age
+age = int(input("enter your age: "))
+adultAge = 18
+if age < adultAge:
+    print("You're not an adult")
+else:
+    print("You're an adult")
