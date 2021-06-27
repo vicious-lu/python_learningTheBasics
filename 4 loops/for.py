0ac4926e3d97802ee0e09263c4021cce4997ed98
vicious-lu <luis.awsm@gmail.com>
2021-06-26 22:24:39 -0600

    add logic and file for for.py loop

--- 4 loops/for.py
+++ 4 loops/for.py
@@ -1,6 +1,14 @@
+#basic structure
 string = 'Hello'
-
-for letter in string:
+for letter in string: #letter is a variable containing each letter form the string, string is an array of letters
     print(letter)
+else:
+    print('end of for cycle\n\n')
+
+#a combination of for and if - else
+for letter in 'Alejandro':
+    if letter == 'a' or letter == 'A':
+        print(f'letter found: {letter}')
+        break #this breaks the for, including the else block
 else:
     print('end of for cycle')
