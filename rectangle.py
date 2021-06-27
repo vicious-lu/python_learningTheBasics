c126456a42b115a7e5e40ae5c1a7a004e342f505
vicious-lu <luis.awsm@gmail.com>
2021-06-26 18:55:12 -0600

    add perimeter function in rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -2,4 +2,6 @@ print("code to calculate the area of a rectangle\n\n")
 h = int(input("height: "))
 w = int(input("widht: "))
 a = h * w
+p = (h + w)*2
 print(f'the area of the rectangle is: {a}')
+print(f'the permiter of the rectangle is: {p}')
