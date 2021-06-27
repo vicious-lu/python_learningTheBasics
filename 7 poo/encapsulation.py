adfea267beb75108bf4c3ce99315ec087d62baf1
vicious-lu <luis.awsm@gmail.com>
2021-06-27 04:07:45 -0600

    add get and set for all atributes of PersonWithGetAndSet

--- 7 poo/encapsulation.py
+++ 7 poo/encapsulation.py
@@ -20,19 +20,36 @@ print('\n\n')
 class PersonWithGetAndSet:
     def __init__(self, name, surname, age) -> None:
         self._name = name # if we want to really protect the data we need to use __ before an atribute
-        self.surname = surname
-        self.age = age
+        self._surname = surname
+        self._age = age
     
     @property  #this is for setting the method name() like an atribute
     def name(self):
         return self._name
 
+    @property
+    def surname(self):
+        return self._surname
+
+    @property
+    def age(self):
+        return self._age
+
     @name.setter #this way we can still change the atribute
     def name(self, name):
         self._name = name
 
+    @surname.setter
+    def surname(self, surname):
+        self._surname = surname
+
+    @age.setter
+    def age(self, age):
+        self._age = age
+    #if we dont add a set method for an atribute, its called a read only atribute
+
     def tellData(self):
-        print(f'name: {self._name} \nsurname: {self.surname} \nage: {self.age}')
+        print(f'name: {self._name} \nsurname: {self._surname} \nage: {self._age}')
 
     pass
 
@@ -40,4 +57,6 @@ person2 = PersonWithGetAndSet('Luis', 'Saucedo', 23)
 print(person2.name) #theres no atribute called name and we still can use it like so
 #now that we have a setter we can change the atribute normally
 person2.name = 'Alejandro'
-print(person2.name)
+person2.surname = 'Devora'
+person2.age = '56'
+print(person2.name, person2.surname, person2.age)
