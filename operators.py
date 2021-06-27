3aacc209abd216fb433eb15a5c718a64213ba6cb
vicious-lu <luis.awsm@gmail.com>
2021-06-26 19:11:16 -0600

    add assignation and comparation operators

--- operators.py
+++ operators.py
@@ -27,3 +27,31 @@ print(f'modulus: {mod}\n\n')
 #exponential
 exp = opA ** opB
 print(f'exponential: {exp}\n\n')
+
+#assignation operator
+print("assingation operator (and increment): ")
+a = 1
+print(a)
+a = a + 1
+print(a)
+a+=1      #better way
+print(a)
+a-=2
+print(a, "\n\n")
+
+#comparation operators
+a = 4
+b = 4
+print(f'a = {a}, b = {b}')
+res = a == b #is a equal to b?
+print(f'a == b  =   {res}')
+res = a != b #is a different than b?
+print(f'a != b  =   {res}')
+res = a > b #is a major than b?
+print(f'a > b   =   {res}')
+res = a >= b #is a major or equal than b?
+print(f'a >= b  =   {res}')
+res = a < b #is a minor than b?
+print(f'a < b   =   {res}')
+res = a <= b #is a minor or equal than b?
+print(f'a <= b  =   {res}')
