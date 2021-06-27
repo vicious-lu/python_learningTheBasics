d6a6b2e23ac4ee17cb7b160fa4f3875f2103cecf
vicious-lu <luis.awsm@gmail.com>
2021-06-26 18:49:40 -0600

    add other operators, -, *, **, /, // and %

--- operators.py
+++ operators.py
@@ -1,5 +1,29 @@
 #A quick reminder of how to use operators
 opA = 3
 opB = 2
-sum = opA + opB
-print(f'resultado suma: {sum}') #testing another syntax
+add = opA + opB
+print(f'addition: {add} \n\n') #testing another syntax
+
+#Substraction
+sub = opA - opB
+print(f'substraction: {sub}\n\n')
+
+#Multiplication
+mul = opA * opB
+print(f'multiplication: {mul}\n\n')
+
+#divition
+div = opA / opB
+print(f'divition: {div}\n\n')
+
+#divition, int
+div = opA // opB
+print(f'divition (int): {div}\n\n')
+
+#modulus
+mod = opA % opB
+print(f'modulus: {mod}\n\n')
+
+#exponential
+exp = opA ** opB
+print(f'exponential: {exp}\n\n')
