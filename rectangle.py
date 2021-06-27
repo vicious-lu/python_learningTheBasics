1463ac359d987ebec0643f60f598ddee1a45cccb
vicious-lu <luis.awsm@gmail.com>
2021-06-26 18:57:04 -0600

    add better title and spacing between prints

--- rectangle.py
+++ rectangle.py
@@ -1,7 +1,7 @@
-print("code to calculate the area of a rectangle\n\n")
+print("code to calculate the area and perimeter of a rectangle\n\n")
 h = int(input("height: "))
 w = int(input("widht: "))
 a = h * w
 p = (h + w)*2
-print(f'the area of the rectangle is: {a}')
+print(f'\n\nthe area of the rectangle is: {a}')
 print(f'the permiter of the rectangle is: {p}')
