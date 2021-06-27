d5075c6789d68196d63892fa307e978ecdf04ef4
vicious-lu <luis.awsm@gmail.com>
2021-06-26 19:30:33 -0600

    mod to a string

--- valueBetweenRange.py
+++ valueBetweenRange.py
@@ -1,9 +1,8 @@
 #code to check if a number is between some given range
 min = 0
 max = 10
-
 value = int(input("enter a number: "))
-if (value >= min) and (value <= max):
+if (value >= min) and (value <= max):           # min <= value <= max
     print(f'the value {value} is in range')
 else:
     print(f'the value {value} is out of range')
