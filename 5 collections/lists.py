0e26be7b8410d0183771ca2eed60e002465d7a0b
vicious-lu <luis.awsm@gmail.com>
2021-06-26 22:40:26 -0600

    add /collections and lists.py in it, add logic for list object and access to index

---
+++ 5 collections/lists.py
@@ -0,0 +1,12 @@
+#define list with names
+names = ['Luis', 'Karla', 'Richard', 'Maria'] #can contain different data types
+
+#printing the list
+print (names) #this prints a list
+
+#access different index in order
+print(names[0])   # -> Luis   
+print(names[1])   # -> Karla
+#inverse order
+print(names[-2])  # -> Richard
+print(names[-1])  # -> Maria
