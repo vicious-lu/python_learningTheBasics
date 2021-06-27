65ca4073428f098de316afd0155c7f9d9a757e4f
vicious-lu <luis.awsm@gmail.com>
2021-06-26 22:20:08 -0600

    add a separator in printNumbers.py and a file for for.py loop, contains logic for for loop:

---
+++ 4 loops/for.py
@@ -0,0 +1,6 @@
+string = 'Hello'
+
+for letter in string:
+    print(letter)
+else:
+    print('end of for cycle')
--- 4 loops/printNumbers.py
+++ 4 loops/printNumbers.py
@@ -4,6 +4,8 @@ while counter <= 10:
     print(counter)
     counter += 1
 
+print('\n\n') #separator
+
 # 5 - 1
 counter = 5
 while counter != 0:
