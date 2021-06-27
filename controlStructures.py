324ee8e9ec8343f249bf6cfb9866fe9156bdc458
vicious-lu <luis.awsm@gmail.com>
2021-06-26 21:27:56 -0600

    add controlStructure and add function basic if else

---
+++ controlStructures.py
@@ -0,0 +1,8 @@
+#basic if-else
+condition = False
+if condition:     #if condition is anything but False or an empty chain will triger the true block
+    print("condition is true")
+elif condition == False:
+    print("condition is False")
+else:
+    print("condition not recognized\n\n")
