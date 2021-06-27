84cad2b98f835d65a0539e17ba967b6d05ecf41f
vicious-lu <luis.awsm@gmail.com>
2021-06-26 22:33:31 -0600

    add logic for the continue word, the range() function in for.py

--- 4 loops/for.py
+++ 4 loops/for.py
@@ -2,13 +2,31 @@
 string = 'Hello'
 for letter in string: #letter is a variable containing each letter form the string, string is an array of letters
     print(letter)
-else:
-    print('end of for cycle\n\n')
+# else:
+#     print('end of for cycle')
+
+print("\n\n")
 
 #a combination of for and if - else
 for letter in 'Alejandro':
     if letter == 'a' or letter == 'A':
         print(f'letter found: {letter}')
         break #this breaks the for, including the else block
-else:
-    print('end of for cycle')
+# else:
+#     print('end of for cycle')
+
+print("\n\n")
+
+#continue and range function
+print('even values:')
+for i in range(6):
+    if i % 2 == 0:
+        print(i)
+    continue # this ignores the rest of the for and goes to the next iteration
+    print('I will never get to console.')
+print('odd values:')
+for i in range(6):
+    if i % 2 != 0:
+        print(i)
+    continue # this ignores the rest of the for and goes to the next iteration
+    print('I will never get to console.')
