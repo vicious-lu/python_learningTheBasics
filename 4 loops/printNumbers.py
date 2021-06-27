bde3cebbbaa9f1e7b710f6ea880701682da426b0
vicious-lu <luis.awsm@gmail.com>
2021-06-26 22:16:29 -0600

    add printNumbers.py with some printing of numbers and while.py with the logic of while itself

---
+++ 4 loops/printNumbers.py
@@ -0,0 +1,11 @@
+# 1 - 10
+counter = 1
+while counter <= 10:
+    print(counter)
+    counter += 1
+
+# 5 - 1
+counter = 5
+while counter != 0:
+    print(counter)
+    counter -= 1
---
+++ 4 loops/while.py
@@ -0,0 +1,15 @@
+#basic while structure, infinite_______________________________
+# cond = True
+# while cond:
+#     print('executing while cycle')
+# else:
+#     print('end of while cycle')
+# print('\n\n') #separator
+
+#print a value three times
+count = 0
+while count < 3:
+    print(count)
+    count += 1
+else:
+    print('end while cycle')
