8123e32cb0655ea7b362adadc3a522535f9f615d
vicious-lu <luis.awsm@gmail.com>
2021-06-26 20:07:40 -0600

    add practice to check the major of two numbers

---
+++ majorTwoNumbers.py
@@ -0,0 +1,11 @@
+#code to check the major between two numbers
+num1 = int(input("enter the first number: "))
+num2 = int(input('enter the second number: '))
+#if num1 > num2:
+#    print('the first number is greater than the second')
+#else:
+#    print('the second number is greater than the first one')
+if num1 > num2:
+    print(f'the major number is: {num1}')
+else:
+    print(f'the major number is: {num2}')
