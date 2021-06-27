dcbfe1c5d6c7c84c8be4371496aa75b5635e11fd
vicious-lu <luis.awsm@gmail.com>
2021-06-26 23:18:37 -0600

    add tuples.py file with the logic for that kind of element, also a little mod in lists.py '

--- 5 collections/lists.py
+++ 5 collections/lists.py
@@ -1,8 +1,8 @@
 #define list with names
 names = ['Luis', 'Karla', 'Richard', 'Maria'] #can contain different data types
-
 #printing the list
-print (names) #this prints a list
+print(names) #this prints the list
+print("\n\n")
 
 #access different index in order
 print(names[0])   # -> Luis   
@@ -10,19 +10,16 @@ print(names[1])   # -> Karla
 #inverse order
 print(names[-2])  # -> Richard
 print(names[-1])  # -> Maria
-
 print("\n\n")
 
 #print a range
 print(f'setting two elements in ranges: \n{names[0:2]}\n')   #Luis(0), Karla(1); without Richard(3)
 print(f'setting one element on the right: \n{names[:3]}\n')    #this way is from the beginning od the list to the 3
 print(f'setting one element at the left: \n{names[1:]}')    #this way is form the index 1 to the end of the list
-
 print("\n\n")
 
 #lenght of a list
 print(len(names))
-
 print("\n\n")
 
 #adding elements to a list
@@ -31,7 +28,6 @@ print(names)        #Now here is Charlie
 #another way to, now telling an index
 names.insert(1, 'Added with insert in 1')
 print(names)        #karla was moved to the right
-
 print("\n\n")
 
 #removing elements from a list
---
+++ 5 collections/tuples.py
@@ -0,0 +1,24 @@
+#defining a tuple, it uses '()' instead of '[]' from a list
+fruits = ("oranges", "bananas", "grapes")
+print(fruits)
+
+#lenght of a tuple
+print(len(fruits))
+print("\n\n")
+
+#with an index
+print(fruits[0])
+print("\n\n")
+#all that we see in lists applies to tuples
+
+#print all elements separated with no format of list
+for tuple in fruits:
+    print(tuple, end = ' ') # we can add whatever we want in "end = ''" to end with between prints
+print("\n\n")
+
+#convert a tuple to a list
+print(fruits)
+listFruits = list(fruits)
+listFruits[0] = 'apples'
+fruits = tuple(listFruits)
+print(fruits)
