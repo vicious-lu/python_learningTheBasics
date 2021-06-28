60516e0300c4ed44fbd342b697ad23b1aa15d911
vicious-lu <luis.awsm@gmail.com>
2021-06-28 15:57:07 -0600

    add function to increment value of class

--- 7 oop/object counter exercise.py
+++ 7 oop/object counter exercise.py
@@ -1,9 +1,14 @@
 class Person:
     person_counter = 0
 
+    @classmethod
+    def generateNextValue(cls):
+        cls.person_counter += 1
+        return cls.person_counter
+
     def __init__(self, name, age) -> None:
         Person.person_counter += 1
-        self.person_id = Person.person_counter
+        self.person_id = Person.generateNextValue()
         self.name = name
         self.age = age
 
